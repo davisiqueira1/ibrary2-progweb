2fb1450d7b3e6b1adb54e7df7b22970a1220ce6a
Davi Siqueira <davisvsiqueira@gmail.com>
2024-12-08 17:54:47 -0300

    feat: add signup page

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.principal, name='principal'),
     path('livros', views.livros, name='livros'),
-    path('livros/detalhes/<int:id>', views.livro_detalhes, name='livro_detalhes')
+    path('livros/detalhes/<int:id>', views.livro_detalhes, name='livro_detalhes'),
+    path('auth/cadastro/', views.cadastro, name='cadastro')
 ]
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UsuarioCreationForm
 
 # Create your views here.
 def principal(request):
@@ -43,3 +46,14 @@ def livro_detalhes(request, id):
         'livro': livro,
     }
     return HttpResponse(template.render(context, request))
+
+def cadastro(request):
+    if request.method == 'POST':
+        form = UsuarioCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('principal')
+    else:
+        form = UsuarioCreationForm()
+    return render(request, 'cadastro.html', {'form': form})
