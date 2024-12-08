909955e4cc1428fa4dcea8222a9818e763844521
Davi Siqueira <davisvsiqueira@gmail.com>
2024-12-08 16:19:04 -0300

    feat: add book details page

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -10,16 +10,36 @@ def livros(request):
     template = loader.get_template('livros.html')
     context = {
         'livros': [
-            {'titulo': 'Dom Casmurro',  'autor': 'Machado de Assis'},
-            {'titulo': 'O Cortiço',  'autor': 'Aluísio Azevedo'},
-            {'titulo': 'Memórias Póstumas de Brás Cubas',  'autor': 'Machado de Assis'},
-            {'titulo': 'A Moreninha',  'autor': 'Joaquim Manuel de Macedo'},
-            {'titulo': 'O Guarani',  'autor': 'José de Alencar'},
-            {'titulo': 'Iracema',  'autor': 'José de Alencar'},
-            {'titulo': 'O Alienista',  'autor': 'Machado de Assis'},
-            {'titulo': 'O Primo Basílio',  'autor': 'Eça de Queirós'},
-            {'titulo': 'O Mulato',  'autor': 'Aluísio Azevedo'},
-            {'titulo': 'O Ateneu',  'autor': 'Raul Pompeia'},
+            {'id': 1, 'titulo': 'Dom Casmurro',  'autor': 'Machado de Assis'},
+            {'id': 2,'titulo': 'O Cortiço',  'autor': 'Aluísio Azevedo'},
+            {'id':3,'titulo': 'Memórias Póstumas de Brás Cubas',  'autor': 'Machado de Assis'},
+            {'id':4,'titulo': 'A Moreninha',  'autor': 'Joaquim Manuel de Macedo'},
+            {'id':5,'titulo': 'O Guarani',  'autor': 'José de Alencar'},
+            {'id':6,'titulo': 'Iracema',  'autor': 'José de Alencar'},
+            {'id':7,'titulo': 'O Alienista',  'autor': 'Machado de Assis'},
+            {'id':8,'titulo': 'O Primo Basílio',  'autor': 'Eça de Queirós'},
+            {'id':9,'titulo': 'O Mulato',  'autor': 'Aluísio Azevedo'},
+            {'id':10,'titulo': 'O Ateneu',  'autor': 'Raul Pompeia'},
         ]
+    }
+    return HttpResponse(template.render(context, request))
+
+def livro_detalhes(request, id):
+    livros = [
+            {'id': 1, 'titulo': 'Dom Casmurro',  'autor': 'Machado de Assis'},
+            {'id': 2,'titulo': 'O Cortiço',  'autor': 'Aluísio Azevedo'},
+            {'id':3,'titulo': 'Memórias Póstumas de Brás Cubas',  'autor': 'Machado de Assis'},
+            {'id':4,'titulo': 'A Moreninha',  'autor': 'Joaquim Manuel de Macedo'},
+            {'id':5,'titulo': 'O Guarani',  'autor': 'José de Alencar'},
+            {'id':6,'titulo': 'Iracema',  'autor': 'José de Alencar'},
+            {'id':7,'titulo': 'O Alienista',  'autor': 'Machado de Assis'},
+            {'id':8,'titulo': 'O Primo Basílio',  'autor': 'Eça de Queirós'},
+            {'id':9,'titulo': 'O Mulato',  'autor': 'Aluísio Azevedo'},
+            {'id':10,'titulo': 'O Ateneu',  'autor': 'Raul Pompeia'},
+    ]
+    livro = livros[id-1]
+    template = loader.get_template('livro_detalhes.html')
+    context = {
+        'livro': livro,
     }
     return HttpResponse(template.render(context, request))
