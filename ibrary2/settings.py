01a2407e7338842dad9ba7e09c4d7c2d1982db31
Davi Siqueira <davisvsiqueira@gmail.com>
2025-01-25 20:03:21 -0300

    style: move from css to bootstrap

--- ibrary2/settings.py
+++ ibrary2/settings.py
@@ -116,13 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/staticfiles/'
+
+import os
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
 
 STATIC_ROOT = BASE_DIR / 'productionfiles'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles'
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
