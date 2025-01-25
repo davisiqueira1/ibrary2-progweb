de2fe2da52e616cfa7ae075031f69c238f938934
Davi Siqueira <davisvsiqueira@gmail.com>
2025-01-25 15:17:08 -0300

    feat: integrate livro page with database

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from .forms import LivroCreationForm
+from .models import Livro
 
 # Create your views here.
 def principal(request):
@@ -16,35 +17,12 @@ def principal(request):
 def livros(request):
     template = loader.get_template('livros.html')
     context = {
-        'livros': [
-            {'id': 1, 'titulo': 'Dom Casmurro',  'autor': 'Machado de Assis'},
-            {'id': 2,'titulo': 'O Cortiço',  'autor': 'Aluísio Azevedo'},
-            {'id':3,'titulo': 'Memórias Póstumas de Brás Cubas',  'autor': 'Machado de Assis'},
-            {'id':4,'titulo': 'A Moreninha',  'autor': 'Joaquim Manuel de Macedo'},
-            {'id':5,'titulo': 'O Guarani',  'autor': 'José de Alencar'},
-            {'id':6,'titulo': 'Iracema',  'autor': 'José de Alencar'},
-            {'id':7,'titulo': 'O Alienista',  'autor': 'Machado de Assis'},
-            {'id':8,'titulo': 'O Primo Basílio',  'autor': 'Eça de Queirós'},
-            {'id':9,'titulo': 'O Mulato',  'autor': 'Aluísio Azevedo'},
-            {'id':10,'titulo': 'O Ateneu',  'autor': 'Raul Pompeia'},
-        ]
+        'livros': Livro.objects.all(),
     }
     return HttpResponse(template.render(context, request))
 
 def livro_detalhes(request, id):
-    livros = [
-            {'id': 1, 'titulo': 'Dom Casmurro',  'autor': 'Machado de Assis'},
-            {'id': 2,'titulo': 'O Cortiço',  'autor': 'Aluísio Azevedo'},
-            {'id':3,'titulo': 'Memórias Póstumas de Brás Cubas',  'autor': 'Machado de Assis'},
-            {'id':4,'titulo': 'A Moreninha',  'autor': 'Joaquim Manuel de Macedo'},
-            {'id':5,'titulo': 'O Guarani',  'autor': 'José de Alencar'},
-            {'id':6,'titulo': 'Iracema',  'autor': 'José de Alencar'},
-            {'id':7,'titulo': 'O Alienista',  'autor': 'Machado de Assis'},
-            {'id':8,'titulo': 'O Primo Basílio',  'autor': 'Eça de Queirós'},
-            {'id':9,'titulo': 'O Mulato',  'autor': 'Aluísio Azevedo'},
-            {'id':10,'titulo': 'O Ateneu',  'autor': 'Raul Pompeia'},
-    ]
-    livro = livros[id-1]
+    livro = Livro.objects.get(id=id)
     template = loader.get_template('livro_detalhes.html')
     context = {
         'livro': livro,
